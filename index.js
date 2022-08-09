30bbc03769247dffaac3c72db0a30141a4d8f7ec
Jordan Jennings <jordanjennings12@gmail.com>
2022-08-09 11:30:21 -0400

    commit tree 1c1814d8ccbf40b556c5215a5abd2dd1f7a03fc9

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+const main = document.getElementById('main');
+main.remove(); 
+
+//const newHeader = document.createElement('header');
+//newHeader.setAttribute('id', 'h1#victory')
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', "victory")
+newHeader.className = "h1#victory";
+
+
+newHeader.textContent = 'YOUR-NAME is the champion';
+
